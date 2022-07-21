71738a2fc62871d6670e87e6739b045dbc8b366c
Alvin Kim <palifarec@gmail.com>
2022-07-20 17:53:25 -0700

    sidebar updated
    CSS Reference: https://gist.github.com/adrianhajdin/900fc7bb6cff0db26a5b6fa7106e7b17

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,16 @@
-import React from "react";
-import ReactDOM from "react";
+// import ReactDOM from "react"; //! older version - before React 18
+import { StrictMode } from "react"; //* New Version
+import { createRoot } from "react-dom/client"; //* New Version
 
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<App />, document.getElementById("root")); //! older version - before React 18
+//* New Version
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
