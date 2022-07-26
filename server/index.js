b8f35b3d82506f373195579fa5c95e11457b0d43
Alvin Kim <palifarec@gmail.com>
2022-07-25 18:58:29 -0700

    form updated, server updated

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+require("dotenv").config(); // This allows us to call the environment variables right inside of node app
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get("/", (req, res) => {
+  res.send("Hello, World");
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
